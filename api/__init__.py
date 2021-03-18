7f245207029175b3224ff9cc6ad7bf00f9d92bde
sukanta-27 <sukanta.raj.27@gmail.com>
2021-03-19 01:35:49 +0530

    Add model imports to app init file

--- api/__init__.py
+++ api/__init__.py
@@ -9,3 +9,13 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 Migrate(app, db)
+
+from api.src.models.AudioFile import AudioFile
+from api.src.models.Song import Song
+from api.src.models.Podcast import Podcast
+from api.src.models.AudioBook import AudioBook
+from api.src.models.Person import Person
+from api.src.models.Host import Host
+from api.src.models.Participant import Participant
+from api.src.models.Author import Author
+from api.src.models.Narrator import Narrator
