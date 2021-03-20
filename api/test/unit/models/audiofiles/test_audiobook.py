577a8568232ab159ec8a1c14e15a92f0bad94df4
sukanta-27 <sukanta.raj.27@gmail.com>
2021-03-20 15:59:07 +0530

    Add more unittests for AudioBook class

--- api/test/unit/models/audiofiles/test_audiobook.py
+++ api/test/unit/models/audiofiles/test_audiobook.py
@@ -23,35 +23,55 @@ class TestAudioBook(TestCase):
         self.assertEqual("TesterNarrator", test.narrator.name)
 
     def test_audioBook_repr(self):
-        test = AudioBook("name", 12, "ss", ["ab", "cd", "ef"])
+        test = AudioBook("name", 12, "author", "narrator")
 
         self.assertEqual(
-            "name: name, Audio type: audioBook, Host:ss, participants: [ab, cd, ef]",
+            "name: name, Audio type: audiobook, Author: author, narrator: narrator",
             test.__repr__()
         )
 
-    def test_audioBook_participants_list_optional(self):
-        test = AudioBook("Test", 12, "ss")
-
-        self.assertEqual(
-            "name: Test, Audio type: audioBook, Host:ss, participants: []",
-            test.__repr__()
-        )
-        self.assertCountEqual([], test.participants)
-
-    def test_audioBook_create_object_with_11_participants_error(self):
-        with self.assertRaises(ValidationError) as e:
-            test = AudioBook("name", 12, host="ss", \
-                participants=["ab", "cd", "ef", "a", "c", "b", "w", "wx", "xxx", "xcv", "xc"])
-            self.assertIn(b"AudioBook cannot have more than 10 participants", str(e))
-
-    def test_audioBook_create_object_without_host_error(self):
+    def test_audioBook_without_author_error(self):
         with self.assertRaises(TypeError) as e:
-            test = AudioBook("name", 12, participants=["ab"])
-            self.assertIn(b"__init__() missing 1 required positional argument: 'host'", str(e))
+            test = AudioBook(name="Test", duration=12, narrator="ss")
+
+            self.assertIn(
+                b"A required field does not have the correct type",
+                str(e)
+            )
+
+    def test_audioBook_without_narrator_error(self):
+        with self.assertRaises(TypeError) as e:
+            test = AudioBook(name="Test", duration=12, author="ss")
+
+            self.assertIn(
+                b"A required field does not have the correct type",
+                str(e)
+            )
+            
+    def test_audioBook_create_object_with_host_object(self):
+        author = Author("Test Author")
+        test = AudioBook(name="Test", duration=12, author=author, narrator="Narrator")
+
+        self.assertEqual("Test", test.name)
+        self.assertEqual(12, test.duration)
+        self.assertIsInstance(test.author, Author)
+        self.assertEqual("Test Author", test.author.name)
+        self.assertIsInstance(test.narrator, Narrator)
+        self.assertEqual("Narrator", test.narrator.name)
+
+    def test_audioBook_create_object_with_host_object(self):
+        narrator = Narrator("Test Narrator")
+        test = AudioBook(name="Test", duration=12, author="Author", narrator=narrator)
+
+        self.assertEqual("Test", test.name)
+        self.assertEqual(12, test.duration)
+        self.assertIsInstance(test.author, Author)
+        self.assertEqual("Author", test.author.name)
+        self.assertIsInstance(test.narrator, Narrator)
+        self.assertEqual("Test Narrator", test.narrator.name)
 
     def test_audioBook_create_object_with_str_duration_error(self):
         with self.assertRaises(TypeError) as e:
-            test = AudioBook("name", "12", host="tester", participants=["ab"])
+            test = AudioBook("name", "12", author="tester", narrator="narrator")
             self.assertIn(b"A required field does not have the correct type", str(e))
 
